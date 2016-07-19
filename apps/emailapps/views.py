37543603419f28cdc42a12a7b6f9768be927e66d
Felisa Deang <felisadee@gmail.com>
2016-07-19 15:09:17 -0700

    another commit. named routes and added reverse method to views for redirects. added client screenshots folder

--- apps/emailapps/views.py
+++ apps/emailapps/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Email
+from django.core.urlresolvers import reverse
 # Create your views here.
 def index(request):
     return render(request, 'emailapps/index.html')
@@ -9,10 +10,10 @@ def process(request):
     if Email.emailManager.isValidEmail(request.POST['email']):
         Email.emailManager.create(email=request.POST['email'])
         messages.success(request, 'The email address you entered ' + request.POST['email']+ ' is a VALID email address! Thank you!')
-        return redirect ('/success')
+        return redirect (reverse('success'))
     else:
         messages.warning(request, 'Invalid email!')
-        return redirect ('/')
+        return redirect (reverse('index'))
 
 def success(request):
     context = {
